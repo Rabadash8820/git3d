78455379c1a1d011871d3176a89cc8a570b8dd49
Dan Vicarel <shundra8820@gmail.com>
2021-08-18 14:59:22 -0400

    All English text has been moved into view data
    ...in anticipation of localization B)

--- build/ViewDataBuilder.ts
+++ build/ViewDataBuilder.ts
@@ -2,6 +2,8 @@ import feather from "feather-icons";
 import npmConfig from "../package.json";
 
 export default class ViewDataBuilder {
+  private static readonly LBL_CLOSE: string = "Close";
+
   public BuildViewData(): Promise<unknown> {
     return Promise.resolve({
       title: "Git 3D",
@@ -12,6 +14,7 @@ export default class ViewDataBuilder {
             "aria-label": "Danger:",
           }),
           message: "Everything's broken!",
+          closeBtnLbl: ViewDataBuilder.LBL_CLOSE,
         },
         {
           level: "warning",
@@ -19,21 +22,30 @@ export default class ViewDataBuilder {
             "aria-label": "Warning:",
           }),
           message: "Everything is probably broken",
+          closeBtnLbl: ViewDataBuilder.LBL_CLOSE,
         },
         {
           level: "info",
           icon: feather.icons["info"].toSvg(),
           message: "Everything should be fine",
+          closeBtnLbl: ViewDataBuilder.LBL_CLOSE,
         },
       ],
       navbar: {
         mainHeading: "Git 3D",
+        toggleBtnLbl: "Toggle navigation",
+        aboutBtnLbl: "About",
+        supportBtnLbl: "Support",
+        sourceBtnLbl: "Source",
         optionsMenuBtnLbl: "Toggle options menu",
         optionsMenuBtnIcon: feather.icons["menu"].toSvg(),
       },
-      optionsMenu: {},
+      optionsMenu: {
+        description:
+          "This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.",
+      },
       footer: {
-        author: npmConfig.author.name,
+        copyright: `Copyright © ${npmConfig.author.name}`,
       },
     });
   }
